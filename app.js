63df16391bb20a7160c20396da69d7bb6eb71cab
mohammadfarheen <46694155+mohammadfarheen@users.noreply.github.com>
2019-03-08 12:03:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
 const express = require('express')
 const app = express()
 
-const hostname = '127.0.0.1'   // set constants
+const hostname = '127.0.0.1'   
 const port = 3002
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+app.post('/', function (req, res) {
+  console.log('POST request')
+  res.send('Welcome back!')
+})
+
+app.use(function (req, res, next) {
+  console.log('logged')
+  next()
 })
 
 app.get('/hello', (req, res) => {
+  console.log('GET request')
   res.send('Hello World!')
 })
 
 app.get('/big',  (req, res) =>{
+  console.log('GET request')
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/greeting/:id',  (req, res) =>{
+  console.log('GET request')
   res.send('Hello! The id was ' + req.params.id)
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
+  console.log('GET request')
   res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
 })
-
-// handle non-existant routes
-app.use((req, res, next) => {
-  res.status(404).send('status 404 - that page was not found');
+ 
+app.get('/one',  (req, res) =>{
+  console.log('GET request')
+  res.send('<h1>The number, ' + req.params.one + ' was selected!</h1>')
 })
 
-app.listen(port, hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}/`)
-  console.log('Hit CTRL-C CTRL-C to stop\n')
+app.get('/two',  (req, res) =>{
+  console.log('GET request')
+  res.send('<h1>The number, ' + req.params.one + ' was selected!</h1>')
+})
+
+app.get('/three',  (req, res) =>{
+  console.log('GET request')
+  res.send('<h1>The number, ' + req.params.one + ' was selected!</h1>')
+})
+
 })
 
